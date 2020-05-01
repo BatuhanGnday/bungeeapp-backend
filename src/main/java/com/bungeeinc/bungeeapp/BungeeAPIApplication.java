b06a1d4359ef37833a341ce1a923df012c4406be
yldrma9 <yldrma9@gmail.com>
2020-05-01 15:40:46 +0300

    Check description
    -Add get posts with tag.

--- src/main/java/com/bungeeinc/bungeeapp/BungeeAPIApplication.java
+++ src/main/java/com/bungeeinc/bungeeapp/BungeeAPIApplication.java
@@ -1,5 +1,7 @@
 package com.bungeeinc.bungeeapp;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.bungeeinc.bungeeapp.database.dao.ITagDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BungeeAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(BungeeAPIApplication.class, args);
+
     }
 }
--- src/main/java/com/bungeeinc/bungeeapp/database/dao/ITagDao.java
+++ src/main/java/com/bungeeinc/bungeeapp/database/dao/ITagDao.java
@@ -1,12 +1,21 @@
 package com.bungeeinc.bungeeapp.database.dao;
 
+import com.bungeeinc.bungeeapp.database.models.Post;
+import org.jdbi.v3.sqlobject.config.RegisterColumnMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ITagDao {
 
     @SqlUpdate("insert into tags (post_id, tag) values (:postId, :tag)")
     @GetGeneratedKeys
     int addTag(@Bind("postId") int postId, @Bind("tag") String tag);
+
+    @SqlQuery("select * from posts inner join tags on post_id = posts.id and tags.tag = :tag")
+    @RegisterColumnMapper(Post.Mapper.class)
+    Page<Post> getPostsWithTag(@Bind("tag") String tag,Pageable pageable);
 }
